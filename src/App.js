2bff58d4d1b2c815d157d058fc3ddf076259a947
cheryltjh <xfreakyn@gmail.com>
2021-10-13 19:39:49 +0800

    css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,35 @@ import Selectcuisine from "./Components/Select_Cuisine";
 import Selecttype from "./Components/Select_Type";
 import Selectresults from "./Components/Select_Results";
 import RandomMeal from "./Components/RandomMeal";
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 3px 3px;
+  margin: 6px 2px;
+  border: 1px solid black;
+  border-radius: 6px;
+  box-sizing: border-box;
+  cursor: pointer;
+  font-size: 16px;
+  @media only screen and (max-width: 600px) {
+    border: 1px solid black;
+    border-radius: 6px;
+    box-sizing: border-box;
+    cursor: pointer;
+    font-size: 14px;
+    position: relative;
+  }
+  &:hover {
+    background-color: rgb(228, 228, 228);
+  }
+  &:active {
+    background-color: grey;
+  }
+`;
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
 
 function App() {
   const [toggle, setToggle] = useState(false);
@@ -27,9 +56,9 @@ function App() {
       <Route exact path="/">
         <Home />
         <section className="controls">
-          <Link to="/select_cuisine">
-            <button>Get Your Recipes!</button>
-          </Link>
+          <LinkStyled to="/select_cuisine">
+            <Button>Get Your Recipes!</Button>
+          </LinkStyled>
         </section>
         <Route path="/RandomMeal">
           <RandomMeal />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,9 +4,11 @@ function Home() {
   return (
     <>
     <h1>Don't know what to eat?</h1>
-  <p>Simply filter for your cuisine preference by clicking on the selections, then click on the meal that you want to find ideas for.</p>
-  <p>A random but relevant recipe will be generated! Do double click on it</p>
-  <p>If what you see doesn’t appeal to you, you can pop back to the recipe generator and keep clicking until you find something that gets your mouth watering.</p>
+  <p>Simply filter for your cuisine preference by clicking on the selections.</p>
+  <p>Click on the meal that you want to find ideas for.</p>
+  <p>A random but relevant recipe will be generated!</p>
+  <p>If what you see doesn’t appeal to you, you can pop back to the recipe generator for more.</p>
+  <p>Keep clicking until you find something that gets your mouth watering!</p>
   </>
   )
 }
